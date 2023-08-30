cb6d3a8d2cc7f9379562e4c6bd26776807d203c7
kyzym <kyzym1@gmail.com>
2023-08-30 16:25:18 +0200

    fix: deploy

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Global styles={GlobalStyles} />
       <Provider store={store}>
         <PersistGate persistor={persistor}>
-          <BrowserRouter basename="/petly-site">
+          <BrowserRouter basename="/">
             <App />
           </BrowserRouter>
         </PersistGate>
